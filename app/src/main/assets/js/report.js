810e5f0fe3cd689b69ce85e9bb9b38ef7b52f856
JimHan978 <jimhan2014@126.com>
2019-03-29 11:56:18 +0800

    登录页

--- app/src/main/assets/js/report.js
+++ app/src/main/assets/js/report.js
@@ -18,118 +18,111 @@
 		}else{
 			result=json;
 		}
-        			var detail = result.detail;
-        			//更新首页
-        			var months = getIntervalMonth(new Date(joinDateStr),new Date());
-        			$("#name").text(detail.name);
-                    $("#nameTitle").text(sex==117?"先生":"女士")
-        			$("#totalMonth").text(months);
+        var detail = result.detail;
+        //更新首页
+        var months = getIntervalMonth(new Date(joinDateStr),new Date());
+        $("#name").text(detail.name);
+        $("#nameTitle").text(sex==117?"先生":"女士")
+        $("#totalMonth").text(months);
 
-        			var joinDate = joinDateStr.split("-");
-        			$("#joinDate").text(joinDate[0] + "年"+joinDate[1] +"月"+joinDate[2] +"日");
+        var joinDate = joinDateStr.split("-");
+        $("#joinDate").text(joinDate[0] + "年"+joinDate[1] +"月"+joinDate[2] +"日");
 
-        			//第二页数据
-        			$("#income_month").text(detail.bysrhj);
+        //第二页数据
+        $("#income_month").text(detail.bysrhj);
 
-        			var historyData = result.historyData;
-        			var xData=[],yData=[],total=0;
-        			for(var i=0;i<historyData.length;i++){
-        				xData[i]=historyData[i].ym.substr(5).replace(/\b(0+)/gi,"")+"月";
-        				yData[i]=historyData[i].bysrhj;
-        				total+=Number(yData[i]);
-        			}
-        			if(historyData.length==1){
-        				$("#accrual").hide();
-        			}else{
-        				$("#accrual").text(detail.bysrhj-yData[historyData.length-2]);
-        				$("#accrual").show();
-        			}
+        var historyData = result.historyData;
+        var xData=[],yData=[],total=0;
+        for(var i=0;i<historyData.length;i++){
+            xData[i]=historyData[i].ym.substr(5).replace(/\b(0+)/gi,"")+"月";
+            yData[i]=historyData[i].bysrhj;
+            total+=Number(yData[i]);
+        }
+        if(historyData.length==1){
+                    $("#accrualDiv").hide();
+                }else{
+                    $("#accrual").text(detail.bysrhj-yData[historyData.length-2]);
+                    $("#accrualDiv").show();
+                }
 
-        			//更新图表
-        			updateIncomeChart(xData,yData);
-        			myChart.resize();
+        //更新图表
+        updateIncomeChart(xData,yData);
+        myChart.resize();
 
-        			//第三页数据
-        			$("#salary").text(detail.zhgzksr);
-        			$("#cut_late").text(detail.cdkk);
-        			$("#cut_leave").text(detail.sjkk);
-        			$("#cut_sick").text(detail.bjkk);
-        			$("#cut_perf").text(detail.jxjc);
-        			$("#award_perf").text(detail.jxjc);
-
-
-        			//第四页数据
-        			$("#housingund").text(detail.gjjlmksr);
-        			$("#income_cash").text(detail.xjflsr);
-        			$("#income_subsidy").text(detail.gdbtksr);
-        			$("#income_bonus").text(detail.jjsr);
-        			$("#income_other").text(detail.qtjlsr);
-        			$("#rank_cash").text(detail.beatXjflsrPercent);
-        			$("#rank_other").text(detail.beatQtjlsrPercent);
+        //第三页数据
+        $("#salary").text(detail.zhgzksr);
+        $("#cut_late").text(detail.cdkk);
+        $("#cut_leave").text(detail.sjkk);
+        $("#cut_sick").text(detail.bjkk);
+        $("#cut_perf").text(detail.jxjc);
+        $("#award_perf").text(detail.jxjc);
 
 
-
-        			//近2个月出勤情况
-        			var isAllWork = true;
-        			//病假
-        			if(detail.bjkk=='0'){
-        				$("#sickleave").remove();
-        				$("#id-sickleave").remove();
-        			}else{
-        				isAllWork = false;
-        			}
-        			//事假
-        			if(detail.sjkk=='0'){
-        				$("#casualleave").remove();
-        				$("#id-casualleave").remove();
-        			}else{
-        				isAllWork = false;
-        			}
-
-        			if(detail.cdkk=='0'){
-        				$("#beLate").remove();
-        				$("#id-beLate").remove();
-        			}else{
-        				isAllWork = false;
-        			}
-
-        			if(!isAllWork){
-        				$("#all-work").remove();
-        				$("#id-all-work").remove();
-        			}
-
-        			//近2个月绩效情况
-        			var perf = Math.sign(Number(detail.jxjc));
-        			switch (perf){
-        				case -1:
-        					$("#qualified").remove();
-        					$("#id-qualified").remove();
-        					$("#reward").remove();
-        					$("#id-reward").remove();
-        					break;
-        				case 1:
-        					$("#qualified").remove();
-        					$("#id-qualified").remove();
-        					$("#dedcut").remove();
-        					$("#id-dedcut").remove();
-        					break;
-        				default:
-        					$("#reward").remove();
-        					$("#id-reward").remove();
-        					$("#dedcut").remove();
-        					$("#id-dedcut").remove();
-        					break;
-        			}
-
-        			$("#cumulativeVal").text(total);
-        			mui(".mui-slider").slider();
+        //第四页数据
+        $("#housingund").text(detail.gjjlmksr);
+        $("#income_cash").text(detail.xjflsr);
+        $("#income_subsidy").text(detail.gdbtksr);
+        $("#income_bonus").text(detail.jjsr);
+        $("#income_other").text(detail.qtjlsr);
+        $("#rank_cash").text(detail.beatXjflsrPercent);
+        $("#rank_other").text(detail.beatQtjlsrPercent);
 
 
-		if (data.success) {
-			
 
-			//mui.init();
-		}
+        //近2个月出勤情况
+        var isAllWork = true;
+        //病假
+        if(detail.bjkk=='0'){
+            $("#sickleave").remove();
+            $("#id-sickleave").remove();
+        }else{
+            isAllWork = false;
+        }
+        //事假
+        if(detail.sjkk=='0'){
+            $("#casualleave").remove();
+            $("#id-casualleave").remove();
+        }else{
+            isAllWork = false;
+        }
+
+        if(detail.cdkk=='0'){
+            $("#beLate").remove();
+            $("#id-beLate").remove();
+        }else{
+            isAllWork = false;
+        }
+
+        if(!isAllWork){
+            $("#all-work").remove();
+            $("#id-all-work").remove();
+        }
+
+        //近2个月绩效情况
+        var perf = Math.sign(Number(detail.jxjc));
+        switch (perf){
+            case -1:
+                $("#qualified").remove();
+                $("#id-qualified").remove();
+                $("#reward").remove();
+                $("#id-reward").remove();
+                break;
+            case 1:
+                $("#qualified").remove();
+                $("#id-qualified").remove();
+                $("#dedcut").remove();
+                $("#id-dedcut").remove();
+                break;
+            default:
+                $("#reward").remove();
+                $("#id-reward").remove();
+                $("#dedcut").remove();
+                $("#id-dedcut").remove();
+                break;
+        }
+
+        $("#cumulativeVal").text(total);
+        mui(".mui-slider").slider();
 	}
 
 	
