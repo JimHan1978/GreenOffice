a90a1f6efdcf3dbdac45797cfe143e47ae1d4d12
PwVoid <954183777@qq.com>
2019-07-18 11:31:20 +0800

    commit

---
+++ app/src/main/java/com/hyetec/moa/model/entity/ActivityEndEntity.java
@@ -0,0 +1,18 @@
+package com.hyetec.moa.model.entity;
+
+public class ActivityEndEntity {
+
+    /**
+     * count : 1
+     */
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
